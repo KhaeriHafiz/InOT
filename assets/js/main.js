9404cf817eab82ee1cd195b1963fe1f64fce74b8
KhaeriHafiz <khaerihafiz6@gmail.com>
2022-06-27 18:00:07 +0800

    login & menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,26 @@ function showing() {
 function closing() {
   document.getElementById("nav-hp").style.display = "none";
 }
+
+(function () {
+  "use strict";
+
+  var treeviewMenu = $(".ul-no");
+
+  // Activate sidebar treeview toggle
+  $("[data-toggle='treeview']").click(function (event) {
+    event.preventDefault();
+    if (!$(this).parent().hasClass("is-expanded")) {
+      treeviewMenu
+        .find("[data-toggle='treeview']")
+        .parent()
+        .removeClass("is-expanded");
+    }
+    $(this).parent().toggleClass("is-expanded");
+  });
+
+  // Set initial active toggle
+  $("[data-toggle='treeview.'].is-expanded")
+    .parent()
+    .toggleClass("is-expanded");
+})();
